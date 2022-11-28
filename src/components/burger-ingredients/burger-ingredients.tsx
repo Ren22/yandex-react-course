bae033f075eb4dcd1d8fcd38615dfa54efd78462
Ren22 <ngm.renat@gmail.com>
2022-11-28 01:52:30 +0100

    sprint 1: adds non working tab element

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import BurgerIngredientsItem from "../burger-ingredients-item/burger-ingredients-item";
 import burgerIngredients from "./burger-ingredients.module.css";
 import { burgersData } from "../../api/burgers";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const BurgerIngredients = () => {
+  const [current, setCurrent] = useState("one");
   const breads = burgersData.filter((b) => b.name.includes("булка"));
   const sauce = burgersData.filter((b) => b.name.includes("Соус"));
   const filling = burgersData.filter(
@@ -11,6 +13,19 @@ const BurgerIngredients = () => {
   );
   return (
     <div className={`${burgerIngredients.main}`}>
+      <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
+      {/* Ниже часть не работает Uncaught TypeError: Cannot read properties of null (reading 'useCallback') */}
+      {/* <div style={{ display: "flex" }}>
+        <Tab value="one" active={current === "one"} onClick={setCurrent}>
+          One
+        </Tab>
+        <Tab value="two" active={current === "two"} onClick={setCurrent}>
+          Two
+        </Tab>
+        <Tab value="three" active={current === "three"} onClick={setCurrent}>
+          Three
+        </Tab>
+      </div> */}
       <h3 className="text text_type_main-medium">Булки</h3>
       <ul className={`${burgerIngredients.list} pt-6 pl-4 pb-10`}>
         {breads.map((b, key) => (
