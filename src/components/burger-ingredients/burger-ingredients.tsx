bec2dfe1429aca3ec58dfccc9be03717d8336f59
Ren22 <ngm.renat@gmail.com>
2022-12-04 18:02:15 +0100

    sprint 1: adds scrollable burger section

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from "react";
 import BurgerIngredientsItem from "../burger-ingredients-item/burger-ingredients-item";
-import burgerIngredients from "./burger-ingredients.module.css";
+import burgerIngredientsStyle from "./burger-ingredients.module.css";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { IngredientDetailsType } from "../../utils/types";
 
@@ -22,39 +22,79 @@ const BurgerIngredients = ({ burgersData }: Props) => {
     () => burgersData?.filter((b) => b.type === "main"),
     [burgersData]
   );
+
   return (
-    <div className={`${burgerIngredients.main} pt-6 pl-4 mr-10`}>
+    <div className={`${burgerIngredientsStyle.main} pt-6 pl-4 mr-10`}>
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
-      <div className={`${burgerIngredients.tabs} pb-10`}>
+      <div
+        onClick={(e) => console.log(e)}
+        className={`${burgerIngredientsStyle.tabs} pb-10`}
+      >
         <Tab value="Breads" active={current === "Breads"} onClick={setCurrent}>
-          Булки
+          <a
+            className={
+              current === "Breads"
+                ? burgerIngredientsStyle.tabs__activeLink
+                : ""
+            }
+            href="#buns"
+          >
+            Булки
+          </a>
         </Tab>
         <Tab value="Sauces" active={current === "Sauces"} onClick={setCurrent}>
-          Соусы
+          <a
+            className={
+              current === "Sauces"
+                ? burgerIngredientsStyle.tabs__activeLink
+                : ""
+            }
+            href="#sauces"
+          >
+            Соусы
+          </a>
         </Tab>
         <Tab
           value="Fillings"
           active={current === "Fillings"}
           onClick={setCurrent}
         >
-          Начинки
+          <a
+            className={
+              current === "Fillings"
+                ? burgerIngredientsStyle.tabs__activeLink
+                : ""
+            }
+            href="#fillings"
+          >
+            Начинки
+          </a>
         </Tab>
       </div>
-      <section className={burgerIngredients.listWrapper}>
+      <section className={burgerIngredientsStyle.listWrapper}>
         <h3 className="text text_type_main-medium">Булки</h3>
-        <ul className={`${burgerIngredients.list} pt-6 pl-4 pb-10`}>
+        <ul
+          id="buns"
+          className={`${burgerIngredientsStyle.list} pt-6 pl-4 pb-10`}
+        >
           {buns.map((b) => (
             <BurgerIngredientsItem key={b._id} ingredientDetails={b} />
           ))}
         </ul>
         <h3 className="text text_type_main-medium">Соусы</h3>
-        <ul className={`${burgerIngredients.list} pt-6 pl-4 pb-10`}>
+        <ul
+          id="sauces"
+          className={`${burgerIngredientsStyle.list} pt-6 pl-4 pb-10`}
+        >
           {sauces.map((s) => (
             <BurgerIngredientsItem key={s._id} ingredientDetails={s} />
           ))}
         </ul>
         <h3 className="text text_type_main-medium">Начинки</h3>
-        <ul className={`${burgerIngredients.list} pt-6 pl-4 pb-10`}>
+        <ul
+          id="fillings"
+          className={`${burgerIngredientsStyle.list} pt-6 pl-4 pb-10`}
+        >
           {fillings.map((f) => (
             <BurgerIngredientsItem key={f._id} ingredientDetails={f} />
           ))}
